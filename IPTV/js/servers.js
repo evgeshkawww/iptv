ca65106cc194de587f20b65c882024b356ff0f55
Evgeshka <nazarov6@gmail.com>
2025-11-01 20:30:03 +0300

    Add files via upload

---
+++ IPTV/js/servers.js
@@ -0,0 +1,218 @@
+window.SPEEDTEST_SERVERS = [
+  {
+    name: "nds.turbod.iptv",
+    server: "https://1.team.ga:8080/",
+    serveropen: "YES",
+    dlURL: "backend/garbage.php",
+    ulURL: "backend/empty.php",
+    pingURL: "backend/empty.php",
+    getIpURL: "backend/getIP.php"
+  },
+  {
+    name: "nds2.turbod.iptv",
+    server: "https://2.team.ga:8080/",
+    serveropen: "YES",
+    dlURL: "backend/garbage.php",
+    ulURL: "backend/empty.php",
+    pingURL: "backend/empty.php",
+    getIpURL: "backend/getIP.php"
+  },
+  {
+    name: "nds3.turbod.iptv",
+    server: "https://3.team.ga:8080/",
+    serveropen: "YES",
+    dlURL: "backend/garbage.php",
+    ulURL: "backend/empty.php",
+    pingURL: "backend/empty.php",
+    getIpURL: "backend/getIP.php"
+  },
+  {
+    name: "nds4.turbod.iptv",
+    server: "https://4.team.ga:8080/",
+    serveropen: "YES",
+    dlURL: "backend/garbage.php",
+    ulURL: "backend/empty.php",
+    pingURL: "backend/empty.php",
+    getIpURL: "backend/getIP.php"
+  },
+  {
+    name: "nds6.turbod.iptv",
+    server: "https://6.team.ga:8080/",
+    serveropen: "YES",
+    dlURL: "backend/garbage.php",
+    ulURL: "backend/empty.php",
+    pingURL: "backend/empty.php",
+    getIpURL: "backend/getIP.php"
+  },
+  {
+    name: "nds7.turbod.iptv",
+    server: "https://7.team.ga:8080/",
+    serveropen: "YES",
+    dlURL: "backend/garbage.php",
+    ulURL: "backend/empty.php",
+    pingURL: "backend/empty.php",
+    getIpURL: "backend/getIP.php"
+  },
+  {
+    name: "nds8.turbod.iptv",
+    server: "https://8.team.ga:8080/",
+    serveropen: "YES",
+    dlURL: "backend/garbage.php",
+    ulURL: "backend/empty.php",
+    pingURL: "backend/empty.php",
+    getIpURL: "backend/getIP.php"
+  },
+  {
+    name: "nds9.turbod.iptv",
+    server: "https://9.team.ga:8080/",
+    serveropen: "YES",
+    dlURL: "backend/garbage.php",
+    ulURL: "backend/empty.php",
+    pingURL: "backend/empty.php",
+    getIpURL: "backend/getIP.php"
+  },
+  {
+    name: "nds10.turbod.iptv",
+    server: "https://10.team.ga:8080/",
+    serveropen: "YES",
+    dlURL: "backend/garbage.php",
+    ulURL: "backend/empty.php",
+    pingURL: "backend/empty.php",
+    getIpURL: "backend/getIP.php"
+  },
+  {
+    name: "nds11.turbod.iptv",
+    server: "https://11.team.ga:8080/",
+    serveropen: "YES",
+    dlURL: "backend/garbage.php",
+    ulURL: "backend/empty.php",
+    pingURL: "backend/empty.php",
+    getIpURL: "backend/getIP.php"
+  },
+  {
+    name: "nds12.turbod.iptv",
+    server: "https://12.team.ga:8080/",
+    serveropen: "YES",
+    dlURL: "backend/garbage.php",
+    ulURL: "backend/empty.php",
+    pingURL: "backend/empty.php",
+    getIpURL: "backend/getIP.php"
+  },
+  {
+    name: "nds13.turbod.iptv",
+    server: "https://13.team.ga:8080/",
+    serveropen: "YES",
+    dlURL: "backend/garbage.php",
+    ulURL: "backend/empty.php",
+    pingURL: "backend/empty.php",
+    getIpURL: "backend/getIP.php"
+  },
+  {
+    name: "nds14.turbod.iptv",
+    server: "https://14.team.ga:8080/",
+    serveropen: "YES",
+    dlURL: "backend/garbage.php",
+    ulURL: "backend/empty.php",
+    pingURL: "backend/empty.php",
+    getIpURL: "backend/getIP.php"
+  },
+  {
+    name: "nds15.turbod.iptv",
+    server: "https://15.team.ga:8080/",
+    serveropen: "YES",
+    dlURL: "backend/garbage.php",
+    ulURL: "backend/empty.php",
+    pingURL: "backend/empty.php",
+    getIpURL: "backend/getIP.php"
+  },
+  {
+    name: "nds16.turbod.iptv",
+    server: "https://16.team.ga:8080/",
+    serveropen: "YES",
+    dlURL: "backend/garbage.php",
+    ulURL: "backend/empty.php",
+    pingURL: "backend/empty.php",
+    getIpURL: "backend/getIP.php"
+  },
+  {
+    name: "nds17.turbod.iptv",
+    server: "https://de.team.ga:8080/",
+    serveropen: "YES",
+    dlURL: "backend/garbage.php",
+    ulURL: "backend/empty.php",
+    pingURL: "backend/empty.php",
+    getIpURL: "backend/getIP.php"
+  },
+  {
+    name: "nds18.turbod.iptv",
+    server: "https://hk.team.ga:8080/",
+    serveropen: "YES",
+    dlURL: "backend/garbage.php",
+    ulURL: "backend/empty.php",
+    pingURL: "backend/empty.php",
+    getIpURL: "backend/getIP.php"
+  },
+  {
+    name: "nds19.turbod.iptv",
+    server: "https://nl.team.ga:8080/",
+    serveropen: "YES",
+    dlURL: "backend/garbage.php",
+    ulURL: "backend/empty.php",
+    pingURL: "backend/empty.php",
+    getIpURL: "backend/getIP.php"
+  },
+  {
+    name: "nds20.turbod.iptv",
+    server: "https://nl2.team.ga:8080/",
+    serveropen: "YES",
+    dlURL: "backend/garbage.php",
+    ulURL: "backend/empty.php",
+    pingURL: "backend/empty.php",
+    getIpURL: "backend/getIP.php"
+  },
+  {
+    name: "nds21.turbod.iptv",
+    server: "https://ru.team.ga:8080/",
+    serveropen: "YES",
+    dlURL: "backend/garbage.php",
+    ulURL: "backend/empty.php",
+    pingURL: "backend/empty.php",
+    getIpURL: "backend/getIP.php"
+  },
+  {
+    name: "nds22.turbod.iptv",
+    server: "https://ru2.team.ga:8080/",
+    serveropen: "YES",
+    dlURL: "backend/garbage.php",
+    ulURL: "backend/empty.php",
+    pingURL: "backend/empty.php",
+    getIpURL: "backend/getIP.php"
+  },
+  {
+    name: "nds23.turbod.iptv",
+    server: "https://ru3.team.ga:8080/",
+    serveropen: "YES",
+    dlURL: "backend/garbage.php",
+    ulURL: "backend/empty.php",
+    pingURL: "backend/empty.php",
+    getIpURL: "backend/getIP.php"
+  },
+  {
+    name: "nds24.turbod.iptv",
+    server: "https://ua.team.ga:8080/",
+    serveropen: "YES",
+    dlURL: "backend/garbage.php",
+    ulURL: "backend/empty.php",
+    pingURL: "backend/empty.php",
+    getIpURL: "backend/getIP.php"
+  },
+  {
+    name: "nds25.turbod.iptv",
+    server: "https://ua2.team.ga:8080/",
+    serveropen: "YES",
+    dlURL: "backend/garbage.php",
+    ulURL: "backend/empty.php",
+    pingURL: "backend/empty.php",
+    getIpURL: "backend/getIP.php"
+  }
+];
