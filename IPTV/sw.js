1a25cdeaa1c0bed3b8a38ecfffc0ac704688bcb2
Evgeshka <nazarov6@gmail.com>
2025-11-25 17:32:08 +0300

    Update sw.js

--- IPTV/sw.js
+++ IPTV/sw.js
@@ -1,35 +1,13 @@
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open('iptv-cache').then(cache => {
-      return cache.addAll([
-        '/bundle.js',
-        '/style.css'
-      ]);
-    })
-  );
+self.addEventListener("install", e => {
+  console.log("SW install");
   self.skipWaiting();
 });
 
-self.addEventListener('activate', event => {
-  event.waitUntil(self.clients.claim());
+self.addEventListener("activate", e => {
+  console.log("SW activate");
+  self.clients.claim();
 });
 
-// Подмена путей
-self.addEventListener('fetch', event => {
-  const url = new URL(event.request.url);
-
-  // Подмена bundle.js → реальный JS
-  if (url.pathname === '/bundle.js') {
-    event.respondWith(fetch('/iptv/IPTV/assets/index-rXCmlGUy.js'));
-    return;
-  }
-
-  // Подмена style.css → реальный CSS
-  if (url.pathname === '/style.css') {
-    event.respondWith(fetch('/iptv/IPTV/assets/index-JXv4NXlQ.css'));
-    return;
-  }
-
-  // Остальные запросы — как обычно
-  event.respondWith(fetch(event.request));
+self.addEventListener("fetch", e => {
+  console.log("SW fetch:", e.request.url);
 });
