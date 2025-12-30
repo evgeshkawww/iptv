7a39397657a086d9f3560f8f673d5c18f8b81e5d
Evgeshka <nazarov6@gmail.com>
2025-12-30 20:59:44 +0300

    Update download_icons.sh

--- download_icons.sh
+++ download_icons.sh
@@ -6,7 +6,9 @@ OUTPUT_DIR="IPTV/ico"
 mkdir -p "$OUTPUT_DIR"
 
 while IFS= read -r url; do
-    filename=$(basename "$url")
-    echo "Скачиваю $url -> $OUTPUT_DIR/$filename"
-    curl -f -L -A "Mozilla/5.0" "$url" -o "$OUTPUT_DIR/$filename" || echo "Ошибка при скачивании $url"
+    # убираем пробелы и \r
+    clean_url=$(echo "$url" | tr -d '\r' | xargs)
+    filename=$(basename "$clean_url")
+    echo "Скачиваю $clean_url -> $OUTPUT_DIR/$filename"
+    curl -f -L -A "Mozilla/5.0" "$clean_url" -o "$OUTPUT_DIR/$filename" || echo "Ошибка при скачивании $clean_url"
 done < "$INPUT_FILE"
