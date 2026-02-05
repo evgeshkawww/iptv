21c227ed1b5ec6f5f4aecde1fefe2b45ea2a7fe8
Evgeshka <nazarov6@gmail.com>
2026-02-05 23:17:00 +0300

    Update update_epg.py

--- IPTV/scripts/update_epg.py
+++ IPTV/scripts/update_epg.py
@@ -1,16 +1,22 @@
 #!/usr/bin/env python3
 import requests
 
-URL = "https://epg.team/5.5.xml.gz?pkg=582,722,262,1002,704,482,822,422,263,862,442,802,522,282,762,1022,63,41"
-OUT_FILE = "IPTV/epg.xml.gz"
+URL_MAIN = "https://epg.team/5.5.xml.gz?pkg=582,722,262,1002,704,482,822,422,263,862,442,802,522,282,762,1022,63,41"
+OUT_MAIN = "IPTV/epg.xml.gz"
 
-def download_epg():
-    print("Скачиваю EPG...")
-    r = requests.get(URL, stream=True, timeout=60)
+URL_EPG7 = "https://iptvx.one/EPG7"
+OUT_EPG7 = "IPTV/epg7.xml.gz"
+
+
+def download(url, out_file, name):
+    print(f"Скачиваю {name}...")
+    r = requests.get(url, stream=True, timeout=60)
     r.raise_for_status()
-    with open(OUT_FILE, "wb") as f:
+    with open(out_file, "wb") as f:
         f.write(r.content)
-    print("EPG обновлён:", OUT_FILE)
+    print(f"{name} обновлён: {out_file}")
+
 
 if __name__ == "__main__":
-    download_epg()
+    download(URL_MAIN, OUT_MAIN, "EPG основной")
+    download(URL_EPG7, OUT_EPG7, "EPG7")
