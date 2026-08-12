9a073793a785ef2650b37f6453f0a7edc505e06e
Evgeshka <nazarov6@gmail.com>
2026-08-12 12:16:31 +0300

    Add files via upload

---
+++ IPTV/scripts/color.js
@@ -0,0 +1,146 @@
+window.SPEEDTEST_SERVERS = [  
+    {
+      name: "s1.turbod.iptv",
+      server: "https://ru2.hls.ga:8080/",
+      dlURL: "backend/garbage.php",
+      ulURL: "backend/empty.php",
+      pingURL: "backend/empty.php",
+      getIpURL: "backend/getIP.php",
+	  serveropen: "YES"
+	},
+    {
+      name: "s3.turbod.iptv",
+      server: "https://16.tvtm.one:8080/",
+      dlURL: "backend/garbage.php",
+      ulURL: "backend/empty.php",
+      pingURL: "backend/empty.php",
+      getIpURL: "backend/getIP.php",
+      serveropen: "YES"
+	},
+    {
+      name: "s5.turbod.iptv",
+      server: "https://ru.tvtm.one:8080/",
+      dlURL: "backend/garbage.php",
+      ulURL: "backend/empty.php",
+      pingURL: "backend/empty.php",
+      getIpURL: "backend/getIP.php",
+      serveropen: "YES"
+    },
+    {
+      name: "s6.turbod.iptv",
+      server: "https://6.hls.ga:8080/",
+      dlURL: "backend/garbage.php",
+      ulURL: "backend/empty.php",
+      pingURL: "backend/empty.php",
+      getIpURL: "backend/getIP.php",
+      serveropen: "YES"
+    },
+    {
+      name: "s7.turbod.iptv",
+      server: "https://ua2.tvtm.one:8080/",
+      dlURL: "backend/garbage.php",
+      ulURL: "backend/empty.php",
+      pingURL: "backend/empty.php",
+      getIpURL: "backend/getIP.php",
+      serveropen: "YES"
+    },
+    {
+      name: "s8.turbod.iptv",
+      server: "https://ru3.tvtm.one:8080/",
+      dlURL: "backend/garbage.php",
+      ulURL: "backend/empty.php",
+      pingURL: "backend/empty.php",
+      getIpURL: "backend/getIP.php",
+      serveropen: "YES"
+    },
+    {
+      name: "s9.turbod.iptv",
+      server: "https://12.tvtm.one:8080/",
+      dlURL: "backend/garbage.php",
+      ulURL: "backend/empty.php",
+      pingURL: "backend/empty.php",
+      getIpURL: "backend/getIP.php",
+      serveropen: "YES"
+    },
+    {
+      name: "s10.turbod.iptv",
+      server: "https://4.hls.ga:8080/",
+      dlURL: "backend/garbage.php",
+      ulURL: "backend/empty.php",
+      pingURL: "backend/empty.php",
+      getIpURL: "backend/getIP.php",
+      serveropen: "YES"
+    },
+    {
+      name: "s11.turbod.iptv",
+      server: "https://6.hls.ga:8080/",
+      dlURL: "backend/garbage.php",
+      ulURL: "backend/empty.php",
+      pingURL: "backend/empty.php",
+      getIpURL: "backend/getIP.php",
+      serveropen: "YES"
+    },
+    {
+      name: "s12.turbod.iptv",
+      server: "https://ru4.hls.ga:8080/",
+      dlURL: "backend/garbage.php",
+      ulURL: "backend/empty.php",
+      pingURL: "backend/empty.php",
+      getIpURL: "backend/getIP.php",
+      serveropen: "YES"
+    },
+    {
+      name: "s13.turbod.iptv",
+      server: "https://9.hls.ga:8080/",
+      dlURL: "backend/garbage.php",
+      ulURL: "backend/empty.php",
+      pingURL: "backend/empty.php",
+      getIpURL: "backend/getIP.php",
+      serveropen: "NO"
+    },
+    {
+      name: "s14.turbod.iptv",
+      server: "https://3.hls.ga:8080/",
+      dlURL: "backend/garbage.php",
+      ulURL: "backend/empty.php",
+      pingURL: "backend/empty.php",
+      getIpURL: "backend/getIP.php",
+      serveropen: "NO"
+    },
+	{
+      name: "s15.turbod.iptv",
+      server: "https://7.hls.ga:8080/",
+      dlURL: "backend/garbage.php",
+      ulURL: "backend/empty.php",
+      pingURL: "backend/empty.php",
+      getIpURL: "backend/getIP.php",
+      serveropen: "NO"
+    },
+	{
+      name: "s16.turbod.iptv",
+      server: "https://ru5.hls.ga:8080/",
+      dlURL: "backend/garbage.php",
+      ulURL: "backend/empty.php",
+      pingURL: "backend/empty.php",
+      getIpURL: "backend/getIP.php",
+      serveropen: "YES"
+    },
+	{
+      name: "s2.turbod.iptv",
+      server: "https://9.hls.ga:8080/",
+      dlURL: "backend/garbage.php",
+      ulURL: "backend/empty.php",
+      pingURL: "backend/empty.php",
+      getIpURL: "backend/getIP.php",
+	  serveropen: "YES"
+    },
+	{
+      name: "s4.turbod.iptv",
+      server: "https://hk.hls.ga:8080/",
+      dlURL: "backend/garbage.php",
+      ulURL: "backend/empty.php",
+      pingURL: "backend/empty.php",
+      getIpURL: "backend/getIP.php",
+      serveropen: "YES"
+    }
+];
